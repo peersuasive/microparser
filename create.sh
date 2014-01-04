7ff407c4bccc297d0ff952c8e151ad2c97551225
Christophe Berbizier (dbdl) <cberbizier@peersuasive.com>
2014-01-04 20:12:23 +0100

    print a status at exit

--- create.sh
+++ create.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+error=0
 usage() {
     echo "Usage: create <input> [LUCE class name] [inherit JUCE class] [output to screen]"
     echo "Environment variable CFLAGS or CXXFLAGS containing -DEFINITIONS for JUCE can be used also"
@@ -7,7 +8,6 @@ file="$1"; cn="$2"; inh="$3"; screen="$4"
 ! [[ -r "$file" ]] && echo "Error: Can't read input file" && usage && exit 1
 
 home=$(dirname "$(readlink -f "${0%}")")
-echo "$home"
 LUA=$(which luajit) || $(which lua) || { echo "found neither lua nor luajit" && exit 1; }
 
 CPP=$(which gcc) || $(which cpp) || $(which clang) || { echo "found neither gcc nor clang" && exit 1; }
@@ -16,11 +16,12 @@ FLAGS="${CFLAGS} ${CXXFLAGS}"
 
 ## clean up file
 out="/tmp/${file##*/}.pre"
-$CPP -E -P $FLAGS "$file" > "$out" || { echo "Failed to preprocess input file"; exit 1; }
+$CPP -E -P $FLAGS "$file" > "$out" || { echo "Failed to preprocess input file"; rm -f "$out"; exit 1; }
 ! [[ -r "$out" ]] && echo "Can't read preprocessed output" && exit 1
 
 ## parse
 export LUA_PATH="$home/?.lua;$(lua -e 'print(package.path)')"
-$LUA "$home/luce_binding.lua" "$out" $cn $inh $screen
+$LUA "$home/luce_binding.lua" "$out" $cn $inh $screen || error=1
 
 \rm -f "$out"
+((!error)) && echo OK || echo KO
